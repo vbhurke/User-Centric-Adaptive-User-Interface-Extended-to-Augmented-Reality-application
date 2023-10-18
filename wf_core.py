d333fc51fd58b6e8f962da6be8422c363c20c140
vbhurke <144071420+vbhurke@users.noreply.github.com>
2023-10-19 04:03:50 +0530

    Update wf_core.py

--- wf_core.py
+++ wf_core.py
@@ -1,6 +1,12 @@
 import subprocess
 import os
+import time
 
-file_name_visualizing=os.getcwd()+"wf_visualiztion.py"
-os.system("wf_dataprocessing.py")
-os.system("wf_visualiztion.py")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the main script
+
+# Run the "wf_dataprocessing.py" script
+subprocess.call(["python", os.path.join(script_dir, "wf_dataprocessing.py")])
+
+# Run the "wf_visualization.py" script
+subprocess.call(["python", os.path.join(script_dir, "wf_visualization.py")])
