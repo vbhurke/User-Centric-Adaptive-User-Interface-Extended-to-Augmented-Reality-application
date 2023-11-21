30d2fef8467738c194cce416a628db54b9271274
vbhurke <144071420+vbhurke@users.noreply.github.com>
2023-11-21 10:26:16 +0530

    Update wf_ml_prediction.py

--- wf_ml_prediction.py
+++ wf_ml_prediction.py
@@ -21,9 +21,9 @@ def kmeans_prediction(df_1, test_data):
     test_data.to_csv("data_processing\df1_test_data.csv", index=False)
     return kmeans_model
 
-def knn_prediction(X_test):
+def knn_prediction(X_test,n):
     # Load the model
-    knn_model = joblib.load('models\knn_model.joblib')
+    knn_model = joblib.load('models\knn_model_'+str(n)+'.joblib')
     # Make predictions on the test set
     y_pred = knn_model.predict(X_test)
     return y_pred
